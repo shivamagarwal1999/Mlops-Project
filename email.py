26cb30b6cb13c9cebf110a3d813fdbb15da2737b
shivam agarwal <43515429+shivamagarwal1999@users.noreply.github.com>
2020-05-23 23:00:11 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,7 +1,7 @@
 import smtplib
-sender = 'shivamagarwal.home@gmail.com'
-receiver = 'shivamagarwal3120@gmail.com'
-passwd ='shivam350'
+sender = 'Enter senders mail id'
+receiver = 'Enter recievers mail id'
+passwd ='Enter senders password'
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 message = "Hello Developer, Your model has been trained Successfully and gives the accuracy= cat /code/accuracy.txt .
